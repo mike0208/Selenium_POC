8b9d79e472e7bb71643f8d4d862f7173ea8e34c3
mayank_gwal <mayankgwal9@gmail.com>
2020-09-12 14:28:04 +0530

    Selenium POC

---
+++ src/main/java/com/ventura/seleniumpoc/App.java
@@ -0,0 +1,38 @@
+package com.ventura.seleniumpoc;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	static WebDriver driver;
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Devops-intell-self-paced\\seleniumpoc\\chromedriver.exe");
+		ChromeOptions  options = new ChromeOptions();
+		options.addArguments("headless");
+		options.addArguments("window-size=1920,1080");
+		
+		driver = new ChromeDriver(options);
+		driver.get("https://www.bing.com/");
+		driver.findElement(By.id("sb_form_q")).sendKeys("Intellipaat");
+
+		WebElement svgObject = driver.findElement(By.xpath("//*[name()='svg']//*[name()='circle']"));
+		Actions builder = new Actions(driver);
+		builder.click(svgObject).build().perform();
+		System.out.println(driver.getTitle());
+		
+		driver.quit();
+
+	}
+
+}
